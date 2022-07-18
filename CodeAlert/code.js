ca69cbb26544dc4e5e03b6817618575f42e6e195
EMT119 <austinarbaugh16@gmail.com>
2022-07-18 18:02:31 -0400

    Updated codeAlert 	modified:   CodeAlert/code.js 	new file:   CodeAlert/codealert.html 	deleted:    CodeAlert/index.html 	modified:   Metronome/metronome.html 	modified:   index.html 	modified:   main.css

--- CodeAlert/code.js
+++ CodeAlert/code.js
@@ -7,17 +7,81 @@ let codeStatus = {
 	},
 	pulseCheck: {
 		minutes: 0,
-		seconds: 1,
+		seconds: 5,
 	},
 };
+
+let currentPulseCheck;
+
 let events = [];
 
-document.getElementById("startCode").addEventListener("click", (e) => {
-	document.getElementById("startCode").disabled = true;
-	const date = new Date();
+const date = new Date();
+
+const formattedDate = () => {
+	let hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+	let minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+	return `${hours}:${minutes}`;
+};
+
+const codeStartBtn = document.getElementById("startCode");
+const codeStopBtn = document.getElementById("stopCode");
+
+codeStartBtn.addEventListener("click", (e) => {
+	codeStartBtn.disabled = true;
+	codeStopBtn.disabled = false;
 	codeStatus.timerRunning = true;
-	events.push(`Code started at ${date.getHours()}:${date.getMinutes()}`);
+	events.push(`Code started at ${formattedDate()}`);
 });
+
+codeStopBtn.addEventListener("click", () => {
+	let reason = prompt("What reason was code stopped? ROSC or Priority 4?");
+	events.push(`Code stopped at ${formattedDate()}. ${reason}`);
+	codeStatus.timerRunning = false;
+});
+
+/*Pulse Check Modal */
+
+const pulseBtn = document.getElementById("pulseCheckPulse");
+const noPulseBtn = document.getElementById("pulseCheckNoPulse");
+
+pulseBtn.addEventListener("click", () => {
+	currentPulseCheck = "Pulse";
+	console.log("Pulse Btn clicked");
+	noPulseBtn.disabled = false;
+	pulseBtn.disabled = true;
+});
+
+noPulseBtn.addEventListener("click", () => {
+	currentPulseCheck = "No pulse";
+	noPulseBtn.disabled = true;
+	pulseBtn.disabled = false;
+});
+
+document.getElementById("saveCheck").addEventListener("click", () => {
+	let rhythm = document.getElementById("cardiacRhythm").value;
+	events.push(`Pulse Check at ${formattedDate()}: ${currentPulseCheck} with ${rhythm}`);
+	document.getElementById("pulseModal").style.display = "none";
+});
+
+document.getElementById("resetCheck").addEventListener("click", () => {
+	codeStatus.pulseCheck.minutes = 0;
+	codeStatus.pulseCheck.seconds = 30;
+	document.getElementById("pulseModal").style.display = "none";
+});
+
+document.querySelectorAll(".defibrillate").forEach((e) => {
+	e.addEventListener("click", (btnValue) => {
+		events.push(`Defibrillation at ${formattedDate()} with ${btnValue.target.value} Joules`);
+	});
+});
+
+document.querySelectorAll(".procedure").forEach((e) => {
+	e.addEventListener("click", (btn) => {
+		events.push(`${btn.target.value} performed at ${formattedDate()}`);
+	});
+});
+
+/*Pulse Check Timer */
 
 setInterval(() => {
 	//Pulse Check Timer
@@ -28,8 +92,10 @@ setInterval(() => {
 			codeStatus.pulseCheck.minutes--;
 			codeStatus.pulseCheck.seconds = 59;
 		} else {
+			pulseBtn.disabled = false;
+			noPulseBtn.disabled = false;
 			document.getElementById("pulseModal").style.display = "block";
-			codeStatus.pulseCheck.minutes = 2;
+			codeStatus.pulseCheck.seconds = 30;
 		}
 
 		let pulseCheckFormat = {
